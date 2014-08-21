840acaec823c645629ac836a1339177cf41b9473
salamandermike <salamandermike@gmail.com>
2014-08-21 09:20:43 -0700

    Fully functional without tags

--- empathologger/app/controllers/eblogs_controller.rb
+++ empathologger/app/controllers/eblogs_controller.rb
@@ -1,2 +1,26 @@
 class EblogsController < ApplicationController
+
+  def index # name of method, tied to name of view file
+    @eblogs = Eblog.all
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    #security workaround
+    # permit the parameters of :author, :title, :content in :eblog
+    eblog = params[:eblog].permit(:author, :title, :content)
+    Eblog.create(eblog)
+    redirect_to "/eblogs"
+  end
+
+  def display
+    id = params[:id]
+    @eblog = Eblog.find(id)
+  end
+
+
+
 end
--- empathologger/config/routes.rb
+++ empathologger/config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
+  root to: 'eblogs#index'
+  get '/eblogs', to: 'eblogs#index'
+  get '/eblogs/new', to: 'eblogs#new'
+  get '/eblogs/:id', to: 'eblogs#display'
+
+  post '/eblogs', to: 'eblogs#create'
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +90,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
