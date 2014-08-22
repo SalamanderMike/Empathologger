f097dbec7b9743644d12b220d106ed48e725ff83
salamandermike <salamandermike@gmail.com>
2014-08-22 08:03:53 -0700

    Comment infrastructure but not working

--- empathologger/app/controllers/comments_controller.rb
+++ empathologger/app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  def index
+    eblog_id = params[:eblog_id]
+    @eblog = Eblog.find(eblog_id)
+    @comments = @eblog.comments
+  end
+
+  def new
+    eblog_id = params[:eblog_id]
+    @eblog = Eblog.find(eblog_id)
+    @comments = @eblog.comments.new
+  end
+
+  def create
+    eblog_id = params[:eblog_id]
+    @eblog = Eblog.find(eblog_id)
+
+    new_comment = params.require(:comment).permit(:title, :body)
+    @comment = @eblog.comments.create(new_comment)
+  end
+
+  def show
+    eblog_id = params[:eblog_id]
+    @eblog = Eblog.find(eblog_id)
+    @comment = @eblog.comments.find(params[:id])
+  end
 end
